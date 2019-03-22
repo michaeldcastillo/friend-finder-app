10a718b586972cb33886be0ee3219aa165f358f7
Mike Castillo Git <michaeldcastillo@hotmail.com>
2019-03-22 18:07:32 -0500

    final tweaks

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,16 +1,26 @@
 var friendsArray = [
   {
-    name: "Donna",
+    name: "Jane Johnson",
     photo: "http://www.personalbrandingblog.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640-300x300.png",
     scores: [4,4,3,1,2,1,3,4,1,1]
   },
   {
-    name: "Ahmed",
+    name: "Maria Hernandez",
     photo: "http://www.personalbrandingblog.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640-300x300.png",
     scores: [3,2,3,4,2,3,3,4,2,3]
   },
   {
-    name: "Jane",
+    name: "Tiara Jackson",
+    photo: "http://www.personalbrandingblog.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640-300x300.png",
+    scores: [1,2,3,1,5,3,1,4,2,1]
+  },
+  {
+    name: "Katie Smith",
+    photo: "http://www.personalbrandingblog.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640-300x300.png",
+    scores: [2,1,3,1,4,2,2,5,2,1]
+  },
+  {
+    name: "Anna Belle",
     photo: "http://www.personalbrandingblog.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640-300x300.png",
     scores: [1,2,5,5,2,5,1,4,2,2]
   }
